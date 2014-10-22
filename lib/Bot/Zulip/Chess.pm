a920c76780625a777ef609c20d308753e41348ca
Jesse Luehrs <doy@tozt.net>
2014-10-22 15:17:28 -0400

    fix undef warning

--- lib/Bot/Zulip/Chess.pm
+++ lib/Bot/Zulip/Chess.pm
@@ -174,7 +174,9 @@ sub draw_state ($self) {
 sub players_turn ($self, $player) {
     my $method = $self->_chessboard->to_move ? 'white_player' : 'black_player';
     my $expected_player = $self->$method;
-    return if !$self->has_black_player && $self->white_player eq $player;
+    return if !$self->has_black_player
+           && $self->has_white_player
+           && $self->white_player eq $player;
     return 1 if !defined($expected_player);
     return 1 if $expected_player eq $player;
     return;
